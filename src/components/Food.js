4cade1424474e5b52aa262a781bef5d9d50ee4a0
ianotieno <ianotieno23@gmail.com>
2023-08-07 20:06:24 +0300

    update filter btn

--- src/components/Food.js
+++ src/components/Food.js
@@ -23,8 +23,8 @@ const Food = () => {
     <div>
     <p className='font-bold text-gray-700'>Filter Type</p>
         <div className='flex justfiy-between flex-wrap'>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+            <button onClick={()=>setFoods(data)} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+            <button onClick={()=>filterType('burger')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
