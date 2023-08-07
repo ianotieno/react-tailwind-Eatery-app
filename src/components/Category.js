3467a133f00b427e6251e51128757e11087409de
ianotieno <ianotieno23@gmail.com>
2023-08-07 20:32:59 +0300

    category grid

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,7 +4,7 @@ const Category = () => {
   return (
     <div className='max-w-[1640px] px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
-      <div>
+      <div className='grid grid-cols-2 md:grid-cols-4'>
         {categories.map((item,index)=>
         <div key={index}>
            <h2>{item.name}</h2>
