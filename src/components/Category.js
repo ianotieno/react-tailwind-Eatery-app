5391f382859196be219bbce09ab10c79e6daadbc
ianotieno <ianotieno23@gmail.com>
2023-08-07 20:25:05 +0300

    category component update

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { categories } from '../data/data'
 const Category = () => {
   return (
     <div>
       <h1>Top Rated Menu Items</h1>
+      <div>
+        {categories.map((item,index)=>
+        <div key={index}>
+
+        </div>
+        )} 
+      </div>
     </div>
   )
 }
