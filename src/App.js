64a3a1b99e27690f8ff7060784b7e66ab43415b4
ianotieno <ianotieno23@gmail.com>
2023-08-07 08:23:50 +0300

    update hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
 
 
 function App() {
   return (
     <div>
        <Navbar/> 
+       <Hero/>
+       <HeadlineCards/>
     </div>
   );
 }
