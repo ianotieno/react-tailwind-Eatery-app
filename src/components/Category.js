6b03858a393235c3fc7b1f71c32490050e5ef0ae
ianotieno <ianotieno23@gmail.com>
2023-08-07 20:39:50 +0300

    category grid

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,7 +9,9 @@ const Category = () => {
         <div key={index} className='bg-gray-100 rounded-lg 
              p-4 flex justify-between items-center'>
            <h2 className='font-bold sm:text-xl'>{item.name}</h2>
-           <img src={item.image} alt={item.name}/>
+           <img src={item.image} alt={item.name}
+           className='w-20'
+           />
         </div>
         )} 
       </div>
