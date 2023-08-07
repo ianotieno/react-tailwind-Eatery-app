4c4bda39ec32883c4ab46521dfb170f1842cad75
ianotieno <ianotieno23@gmail.com>
2023-08-07 08:41:36 +0300

    update food  component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HeadlineCards from './components/HeadlineCards';
+import Food from './components/Food';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
        <Navbar/> 
        <Hero/>
+       <Food/>
        <HeadlineCards/>
     </div>
   );
--- src/components/Food.js
+++ src/components/Food.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import {data} from '../data/data.js'
 const Food = () => {
+    console.log(data)
   return (
     <div>
-      
+      <h1>Top Rated Menu Items</h1>
+      <div>
+        <div>
+    <p>Filter Type</p>
+        <div>
+            <button>All</button>
+            <button>Burgers</button>
+            <button>Pizza</button>
+            <button>Salads</button>
+            <button>Chicken</button>
+        </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
