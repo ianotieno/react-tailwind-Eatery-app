55d0fa9bd1b1da553eff587414bfb64c6a315693
ianotieno <ianotieno23@gmail.com>
2023-08-07 20:27:16 +0300

    category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -7,7 +7,8 @@ const Category = () => {
       <div>
         {categories.map((item,index)=>
         <div key={index}>
-
+           <h2>{item.name}</h2>
+           <img src={item.image} alt={item.name}/>
         </div>
         )} 
       </div>
