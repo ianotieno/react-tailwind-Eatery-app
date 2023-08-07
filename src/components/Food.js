26b1b43aa77d64a9f030501ac1004627091bfaed
ianotieno <ianotieno23@gmail.com>
2023-08-07 15:45:59 +0300

    update map array

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../data/data.js'
 const Food = () => {
-    console.log(data)
+    const[foods, setFoods]=useState(data);
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
       <div className='flex flex-col lg:flex-row justify-between'>
-        <div>
+    <div>
     <p className='font-bold text-gray-700'>Filter Type</p>
         <div className='flex justfiy-between flex-wrap'>
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
@@ -15,7 +15,7 @@ const Food = () => {
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
             <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
         </div>
-        
+       </div> 
         <div>
           <p className='font-bold text-gray-700'>Filter Price</p>
           <div className='flex justify-between max-w-[390px] w-ful'>
@@ -26,6 +26,13 @@ const Food = () => {
           </div>
         </div>
       </div>
+      
+      <div>
+      {foods.map((item, index) => (
+          <div>
+            <img src={item.image} alt={item.name}/>
+          </div>
+        ))}
       </div>
     </div>
   )
